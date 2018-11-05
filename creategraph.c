5a69db406d8cd11999690db183018358f8dadd0c
Kathryn Thiese <40401873+kthiese@users.noreply.github.com>
2018-11-05 15:58:54 -0600

    Add files via upload

--- creategraph.c
+++ creategraph.c
@@ -4,23 +4,46 @@
 
 // Constructs the graph
 int createGraph(Info** targets, int tsize) {
-	struct Graph* graph = createGraph(tsize);
+	struct NodeList* graph = createList(tsize);
 	
-	// For each target
-	for (int i = 0; i < tsize; i++){
-		// check if one of the dependencies
-		for (int j = 0; j < targets[i]->dpsize; j++) {
-		       // is also a target. If yes, then add an edge from that
-		       // target to that 'dependency' (which is actually another target)
-			for (int k = 0; k < tsize; k++) {
-				if (k == i) {
-					continue;
-				} else if (strcmp(targets[k].target, targets[i]->dps[j]) == 0) {
-					addEdge(graph, i, targets[k]);
-				}
-		       }
-		}	       
+	// Add each target to graph, making a linked list of the targets
+	for (int i = 0; i < tsize; i++) {
+		addTarget(graph, targets[i]);
 	}
+
+	// For each node in the graph, add the children.
+	struct Node* curr = graph->head;
+	int v = 0;
+	for (i = 0; i < tsize; i++) {
+		curr = curr->next;
+		curr->children = (struct Node *)malloc(sizeof(struct Node*) * curr->info->dpssize);
+		for (int j = 0; j < curr->info->dpsize; j++){
+			for (int k = 0; k < tsize; k++) {
+				if (k == i){
+					continue;
+				} else if (strcmp(curr->info->dps[j], targets[k]->target) == 0) {
+					curr->children[v++] = findTarget(graph, targets[k]->target);
+				}
+			}
+		}
+		v = 0;
+	}
+
+//	// For each target
+//	for (int i = 0; i < tsize; i++){
+//		// check if one of the dependencies
+//		for (int j = 0; j < targets[i]->dpsize; j++) {
+//		       // is also a target. If yes, then add an edge from that
+//		       // target to that 'dependency' (which is actually another target)
+//			for (int k = 0; k < tsize; k++) {
+//				if (k == i) {
+//					continue;
+//				} else if (strcmp(targets[k].target, targets[i]->dps[j]) == 0) {
+//					addEdge(graph, i, targets[k]);
+//				}
+//		       }
+//		}	       
+//	}
 	
 	// Pass the graph to actually run the makefile. 
 	runMake(graph);
--- graph.c
+++ graph.c
@@ -6,50 +6,88 @@
 struct Info {
 	int dpsize;
 	char* target;
-	char** cmds;
+	struct Command* cmds;
 	char** dps;
+}
+
+// Struct that holds a command
+struct Command {
+	char* cmd;
+	char** args;
 }
 
 // Struct for a node in the graph, holding a target and the next node
 struct Node {
 	struct Info* info;
+	struct Node** children;
 	struct Node* next;
 }
 
 // List of nodes for the graph
 struct NodeList {
+	int size;
 	struct Node *head;
 }
 
-// Graph struct holding multiple list of nodes
-struct Graph{
-	int size;
-	struct NodeList* list;
-}
-
-// Create a node by passing in info for the target
+// Create a node given the info struct
 struct Node* createNode(struct * Info info) {
 	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node*));
 	newNode->info = info;
+	newNode->children = NULL;
 	newNode->next = NULL;
 	return newNode;
 }
 
-// Initialize graph
-struct Graph* createGraph(int size) {
-	struct Graph *graph  = (struct Graph*)malloc(sizeof(struct * Graph));
-	graph->size = size;
-	graph->list = (struct NodeList*)malloc(size*sizeof(struct NodeList*));
-	for (int i = 0; i < size; i++){
-		graph->list[i].head = NULL;
-	}
-
-	return graph;
+// Create a list
+struct NodeList* createList(int tsize){
+	struct NodeList* newlist = (struct NodeList*)malloc(sizeof(struct NodeList*));
+	newlist->head->info = NULL;
+	newlist->head->children = NULL;
+	newlist->head->next=NULL;
+	newlist->size = tsize;
+	return newlist;
 }
+
+// Add target to list
+void addTarget(struct NodeList* list, Info* info){
+	struct Node* curr = list->head;
+	while (curr->next != NULL) {
+		curr = curr->next;	
+	}	
+	struct Node* newnode = createNode(info);
+	curr->next = newnode;
+}
+
+// Finds a node with target name parameter
+struct Node* findTarget(struct NodeList* list, char* target) {
+	struct Node* curr = list->head;
+	while (curr->next != NULL && strcmp(curr->next->info->target, target) !- 0) {
+		curr = curr_.next;
+	}
+	return curr->next;
+}
+
+// Graph struct holding multiple list of nodes
+//struct Graph{
+//	int size;
+//	struct NodeList* list;
+//}
+
+// Initialize graph
+//struct Graph* createGraph(int size) {
+//	struct Graph *graph  = (struct Graph*)malloc(sizeof(struct * Graph));
+//	graph->size = size;
+//	graph->list = (struct NodeList*)malloc(size*sizeof(struct NodeList*));
+//	for (int i = 0; i < size; i++){
+//		graph->list[i].head = NULL;
+//	}
+//
+//	return graph;
+//}
 
 // Add an edge between a target and another target in the graph. 
-void addEdge(struct Graph*, int src, struct Info* info) {
-	struct Node* newNode = createNode(info);
-	newNode->next = graph->list[src].head;
-	graph->list[src].head = newNode;
-}
+//void addEdge(struct Graph*, int src, struct Info* info) {
+//	struct Node* newNode = createNode(info);
+//	newNode->next = graph->list[src].head;
+//	graph->list[src].head = newNode;
+//}
